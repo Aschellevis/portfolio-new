51e173b39717f2bd455cb7dbb5edaecf6077c77f
Aschellevis <94119935+Aschellevis@users.noreply.github.com>
2023-02-02 19:14:02 +0100

    Fixed navigation

--- index.php
+++ index.php
@@ -14,9 +14,6 @@ include_once('templates/head.php');
             </div>
         </div>
     </main>
-    <div class="container-md">
-        <div class="row projects p-0"></div>
-    </div>
     <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
