b18ddd33a68d499b26ec1f383ee4fe8d320c0c95
Aschellevis <94119935+Aschellevis@users.noreply.github.com>
2023-02-03 17:16:21 +0100

    Tried to fix active navbar link

--- templates/head.php
+++ templates/head.php
@@ -6,7 +6,7 @@
     <title>Portfolio</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="custom.css" />
+    <link rel="stylesheet" type="text/css" href="css/custom.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.1/font/bootstrap-icons.css">
 
 </head>
