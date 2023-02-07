867497f28126a9e482c99ee96aa74142cbb14f0e
Aschellevis <94119935+Aschellevis@users.noreply.github.com>
2023-02-07 16:59:22 +0100

    Modal boxes work and are filled.

--- js/main.js
+++ js/main.js
@@ -1,57 +1,19 @@
-// all the arrays for the projects
-// const project = document.querySelector('#projects');
-// const projectsArray = [];
+document.addEventListener('click', function (e) {
+    e = e || window.event;
+    var target = e.target || e.srcElement;
 
-// let healthOne = {
-//     title: 'Health One',
-//     image: '../img/projects/healthone/healthone.jpg',
-//     github: 'https://github.com/Aschellevis/healthone'
-// }
+    if (target.hasAttribute('data-toggle') && target.getAttribute('data-toggle') == 'modal') {
+        if (target.hasAttribute('data-target')) {
+            var m_ID = target.getAttribute('data-target');
+            document.getElementById(m_ID).classList.add('open');
+            e.preventDefault();
+        }
+    }
 
-// let jumper = {
-//     title: 'Infinite jumper',
-//     image: '../img/projects/jumper.jpg',
-//     github: 'https://github.com/ROCMondriaanTIN/sd-21-kd-gp-infinite-jumper-Aschellevis'
-// }
-
-// let generator = {
-//     title: 'Quote generator',
-//     image: '../img/projects/quotegenerator.jpg',
-//     github: '#'
-// }
-
-// // put the arrays in one const
-// projectsArray.push(healthOne, jumper, generator);
-// console.log(projectsArray)
-
-// // show the arrays on the page
-// for (let i = 0; i < projectsArray.length; i++){
-//     const projects = projectsArray[i];
-//     $('#content').html('<div class="col-md-4">' + 
-//     '<div class="card">' + '<img src="' + projects.image + '" class="card-img-top" alt="project">' +
-//     '<div class="card-body">' + '<h5 class="card-title">' + projects.title + '</h5>' +
-//     '<p><a href="' + projects.github + '"><i class="bi bi-github"></i></a></p>' +
-//     '</div>' + '</div>' + '</div>');
-//     console.log(projects.title)
-// }
-
-//jQuery codes
-$(document).ready(function(){
-
-    // // show home page
-    // $(document).on('click', '#home', function(){
-
-    //     var html = `
-                
-    //             `;
-
-    //     $('#content').html(html);
-    // });
-
-    // Add active class to the current button (highlight it)
-    $('li').on(click, function() { 
-        $(this).siblings().removeClass('active');
-        $(this).addClass('active');
-    });
-
-});
+    // Close modal window with 'data-dismiss' attribute or when the backdrop is clicked
+    if ((target.hasAttribute('data-dismiss') && target.getAttribute('data-dismiss') == 'modal') || target.classList.contains('modal')) {
+        var modal = document.querySelector('[class="modal open"]');
+        modal.classList.remove('open');
+        e.preventDefault();
+    }
+}, false);
